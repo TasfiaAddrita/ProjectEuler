d59200563a32b688fd99264a6c43c478ddfa59b0
Tasfia Addrita <tasfia.addrita@gmail.com>
2015-03-24 23:40:02 -0400

    I'm stupid

--- sumOfMultiplesOf3and5.js
+++ sumOfMultiplesOf3and5.js
@@ -1,43 +1,11 @@
-function sumOfMultiplesOf3and5() {
+function multiples() {
+	var total = 0;
 
-	a = 0;
-	b = 0;
-	c = 0;
-	var sum = 0;
-	var multiples = [];
-	var mulOf3 = [];
-	var mulOf5 = [];
-
-	for (i = 0; i < 1000; i++) {
-		if (i % 3 === 0 &&  i % 5 === 0) {
-			multiples[a] = i;
-			a++;
-		}
-
-		else if (i % 3 === 0) {
-			mulOf3[b] = i;
-			b++; 
-		} 
-
-		else if (i % 5 === 0) {
-			mulOf5[c] = i;
-			c++;
-		}
+	for(var i = 0; i < 1000; i++) {
+		if(i % 3 === 0 || i % 5 === 0) {
+	    	total += i;
+	    }
 	}
-	
-	for (i = 0; i < multiples.length - 1; i++) {
-		sum += multiples[i];
-	}
-	
-	for (i = 0; i < mulOf3.length - 1; i++) {
-		sum += mulOf3[i];
-	}
-
-	for (i = 0; i < mulOf5.length - 1; i++) {
-		sum += mulOf5[i];
-	}
-	
-	console.log(sum);
+	console.log(total);
 }
 
-sumOfMultiplesOf3and5();
