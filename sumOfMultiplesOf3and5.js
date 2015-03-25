c41ddd25c0bdcc40af54e0acd6071b36470e8452
Tasfia Addrita <tasfia.addrita@gmail.com>
2015-03-24 22:38:35 -0400

    Created function and loop

--- sumOfMultiplesOf3and5.js
+++ sumOfMultiplesOf3and5.js
@@ -2,6 +2,7 @@ function sumOfMultiplesOf3and5() {
 
 	a = 0;
 	b = 0;
+	var sum = 0;
 	var mulOf3 = [];
 	var mulOf5 = [];
 
@@ -16,7 +17,16 @@ function sumOfMultiplesOf3and5() {
 			b++;
 		}
 	}
-
-	console.log(mulOf3); 
-	console.log(mulOf5); 
+	
+	for (i = 0; i < mulOf3.length - 1; i++) {
+		sum += mulOf3[i];
+	}
+	
+	for (i = 0; i < mulOf5.length - 1; i++) {
+		sum += mulOf5[i];
+	}
+	
+	console.log(sum);
 }
+
+sumOfMultiplesOf3and5();
