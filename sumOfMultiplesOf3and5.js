2e3872b68c4d917341616f23da3cad6e43a1d572
Tasfia Addrita <tasfia.addrita@gmail.com>
2015-03-24 22:15:38 -0400

    developing algorithm

--- sumOfMultiplesOf3and5.js
+++ sumOfMultiplesOf3and5.js
@@ -1,5 +1,22 @@
 function sumOfMultiplesOf3and5() {
+
+	a = 0;
+	b = 0;
+	var mulOf3 = [];
+	var mulOf5 = [];
+
 	for (i = 0; i < 1000; i++) {
-		
+		if (i % 3 === 0) {
+			mulOf3[a] = i;
+			a++; 
+		} 
+
+		if (i % 5 === 0) {
+			mulOf5[b] = i;
+			b++;
+		}
 	}
+
+	console.log(mulOf3); 
+	console.log(mulOf5); 
 }
