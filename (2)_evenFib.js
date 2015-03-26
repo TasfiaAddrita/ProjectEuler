f40257fc8c527a51cdf2c71280482640b8ad4adf
Tasfia Addrita <tasfia.addrita@gmail.com>
2015-03-25 20:13:18 -0400

    added headers and worked on evenFib

--- (2)_evenFib.js
+++ (2)_evenFib.js
@@ -1,3 +1,18 @@
-function evenFib() {
-	
+var a = 1;
+var b = 2; 
+var sum;
+var sumOfEven = 0;
+
+for (i = 1; i < 100; i++) {
+	sum = a + b;
+
+	if (sum % 2 === 0 && sum < 4000000) {
+		sumOfEven = sumOfEven + sum;
+	}
+
+	a = b;
+	b = sum;
 }
+
+console.log(sum)
+console.log(sumOfEven);
